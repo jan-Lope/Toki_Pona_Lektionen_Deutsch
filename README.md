09d6ad10d3c070257b44c9b87ef4251febca7b49
me <email@example.com>
2017-07-25 20:05:16 +0200

    online dictionary

--- README.md
+++ README.md
@@ -6,18 +6,20 @@ den Lektionen von jan Pije ( [tokipona.net/tp/janpije](http://tokipona.net/tp/ja
 
 ## Generierte PDF- HTML- und TXT-Dateien
 
-[toki-pona-lessons_de.pdf](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/blob/gh-pages/toki-pona-lessons_de.pdf)  
-Generiertes Buch (A4)  
+### Lektionen 
 
-[toki-pona-lessons_de-booklet.pdf](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/blob/gh-pages/toki-pona-lessons_de-booklet.pdf)  
-Generieretes Booklet (A5). 
+[toki-pona-lessons_de.pdf](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/blob/gh-pages/toki-pona-lessons_de.pdf) Generiertes Buch (A4)  
+
+[toki-pona-lessons_de-booklet.pdf](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/blob/gh-pages/toki-pona-lessons_de-booklet.pdf) Generieretes Booklet (A5). 
 Bei der Booklet-Version werden jeweils zwei Seiten auf eine A4-Seite abgebildet, so dass ein daraus ein Booklet (A5) erstellt werden kann. Dazu werden erst alle ungeraden Seiten ausgedruckt. Dann legt man die Blätter wieder ins Papierfach zurück und druckt alle geraden Seiten in absteigender Reihenfolge aus. 
 
-[HTML-Version](https://htmlpreview.github.io/?https://raw.githubusercontent.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/gh-pages/toki-pona-lessons_de/index.html)  
+[HTML-Version](https://htmlpreview.github.io/?https://raw.githubusercontent.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/gh-pages/toki-pona-lessons_de/index.html) Generierte Online-Version.
 
-[toki-pona_deutsch.txt](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/blob/gh-pages/toki-pona_deutsch.txt)  
-Generiertes Wörterbuch  
-Dieses Wörterbuch kann mit der Software [ding](http://www-user.tu-chemnitz.de/~fri/ding/) verwendet werden.
+### Dictionary
+
+
+
+[toki-pona_deutsch.txt](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch/blob/gh-pages/toki-pona_deutsch.txt) Generiertes Text-Datei für die Software [ding](http://www-user.tu-chemnitz.de/~fri/ding/).
 
 ![ding](ding01.png?raw=true "ding")
 
@@ -26,13 +28,11 @@ Dieses Wörterbuch kann mit der Software [ding](http://www-user.tu-chemnitz.de/~
 ![ding](ding03.png?raw=true "ding")
 
 
-
 ### Source Codes
 
 Die PDF-, HTML- und TXT-Dateien werden automatisch aus den Latex-Dateien per [travis-ci.org](https://travis-ci.org/jan-Lope/Toki_Pona_Lektionen_Deutsch) generiert.
 
 Die Latex-Datei und die anderen Dateien sind in [Github](https://github.com/jan-Lope/Toki_Pona_Lektionen_Deutsch) veröffentlicht.  
-
 
 ### Manuelles Generieren
 
@@ -46,7 +46,8 @@ Das manuelle Generieren der PDF- und HTML- und TXT-Dateien kann unter Ubuntu erf
 Lade alle Dateien in ein Verzeichnis und wechsle in diese Verzeichis. Unter Linux (Ubuntu) starte folgendes Script:
 
 
-    ./make_booklet.sh
+    ./make.sh
+
 
 
   
--- make.sh
+++ make.sh
@@ -63,18 +63,6 @@ if [ ! -f $TEX_FILE-booklet.pdf ]; then
 	echo "ERROR"
 	exit 1
 fi
-echo "make toki-pona-dictionary"
-latex toki-pona-dictionary.tex > /dev/null  2> /dev/null
-if [ ! -f toki-pona-dictionary.dvi ]; then
-	echo "ERROR"
-	exit 1
-fi
-pdflatex toki-pona-dictionary.tex > /dev/null 2> /dev/null
-if [ ! -f toki-pona-dictionary.pdf ]; then
-	echo "ERROR"
-	exit 1
-fi
-
 echo "make html files"
 latex2html $TEX_FILE.tex > /dev/null 2> /dev/null
 if [ ! -f $TEX_FILE/index.html ]; then
@@ -95,6 +83,7 @@ for i in `ls $TEX_FILE/*.html` ; do
           sed -e '1,$s/ BORDER=\"1\"/ BORDER=\"0\"/g' $i > $i.neu && mv $i.neu $i
           sed -e '1,$s/ VALIGN=\"TOP\" WIDTH=5/ VALIGN=\"TOP\" WIDTH=\"300\"/g' $i > $i.neu && mv $i.neu $i
 done
+#
 echo "Create Build directory and copy the pdf and html files in this directory."
 rm -rf _build
 mkdir -p _build
@@ -104,10 +93,6 @@ if [ ! -f _build/$TEX_FILE.pdf ]; then
 	exit 1
 fi
 if [ ! -f _build/$TEX_FILE-booklet.pdf ]; then
-	echo "ERROR"
-	exit 1
-fi
-if [ ! -f _build/toki-pona-dictionary.pdf ]; then
 	echo "ERROR"
 	exit 1
 fi
@@ -149,8 +134,7 @@ rm -f *.ind
 rm -f *.ist
 #
 echo "make word list for ding dictionary ( http://www-user.tu-chemnitz.de/~fri/ding/ )"
-fgrep "&&" *.tex | fgrep "\\" | fgrep -v "%" | fgrep -v "% no-dictionary" | fgrep -v "% & English - Toki Pona" | cut -d: -f2- | awk -F\& '{
-print $1 "::" $3 $4 $5 $6 $7 }'  > tmp.txt
+fgrep "&&" *.tex | fgrep "\\" | fgrep -v "%" | fgrep -v "% no-dictionary" | fgrep -v "% & English - Toki Pona" | cut -d: -f2- | awk -F\& '{print $1 "::" $3 $4 $5 $6 $7 }'  > tmp.txt
 fgrep "&&" *.tex | fgrep "\\" | fgrep "% & English - Toki Pona" | cut -d: -f2- | awk -F\& '{print $3 ":: " $1 }'  >> tmp.txt
 if [ ! -f tmp.txt ]; then
 	echo "ERROR"
@@ -168,6 +152,9 @@ sed -e 's#'\glqq'#''#g' tmp.txt > tmp.neu && mv tmp.neu tmp.txt
 sed -e 's#'\grqq'#''#g' tmp.txt > tmp.neu && mv tmp.neu tmp.txt
 sed -e 's#\\#''#g' tmp.txt > tmp.neu && mv tmp.neu tmp.txt
 sed -e 's#^ #''#g' tmp.txt > tmp.neu && mv tmp.neu tmp.txt
+expand tmp.txt > tmp.neu && mv tmp.neu tmp.txt                          # replace tabs with spaces
+sed -e 's/  */ /g' tmp.txt > tmp.neu && mv tmp.neu tmp.txt              # strip multiple spaces to one
+sed -e 's/[ \t]*$//' "$1" tmp.txt > tmp.neu && mv tmp.neu tmp.txt       # strip trailing whitespace
 echo "## $TODAY Automatically generated from the Toki Pona lessons. https://github.com/jan-Lope/" >  toki-pona_deutsch.txt
 echo "## Diese Datei ist verwendbar mit der software ding ( http://www-user.tu-chemnitz.de/~fri/ding/ ). " >> toki-pona_deutsch.txt
 cat tmp.txt | sort | uniq >> toki-pona_deutsch.txt
@@ -186,7 +173,6 @@ if [ $? != 0  ]; then
 	echo "ERROR"
 	exit 1
 fi
-
 mv toki-pona_deutsch.txt _build
 fgrep "mi moku" _build/toki-pona_deutsch.txt > /dev/null 2> /dev/null
 if [ $? != 0  ]; then
@@ -194,19 +180,33 @@ if [ $? != 0  ]; then
 	exit 1
 fi
 #
+echo "make dictionary.coffee"
+cat dictionary-head.coffee > _build/dictionary.coffee
+# expand _build/toki-pona_english.txt | fgrep -v "##" | sed -e 's#'\''#''#g' | sed -e 's/  */ /g'  >> _build/dictionary.coffee
+expand _build/toki-pona_deutsch.txt | fgrep -v "##" | sed -e 's/  */ /g'  >> _build/dictionary.coffee
+cat dictionary-tail.coffee >>  _build/dictionary.coffee
+#
+echo "make dictionary.js"
+# coffeelint _build/dictionary.coffee
+coffee -c _build/dictionary.coffee
+if [ $? != 0  ]; then
+	echo "ERROR"
+	exit 1
+fi
+# coffee -v
+rm -f _build/dictionary.coffee
+cp dictionary.html _build/
+#
 echo " "
-echo "The pdf, html and txt files are in the directory _build."
+echo "The generated files are in the directory _build."
 ls _build
 echo " "
 echo "Printing of the booklet: "
 echo "Print the odd numbers first."
 echo "Put the paper with the script on the top again in the printer."
-echo " "
-echo "end $0"
+# echo " "
+# echo "end $0"
 echo "########################################################################"
-echo " "
-
-#
-###############################################################################
+# echo " "
 ###############################################################################
 # eof
